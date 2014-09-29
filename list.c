e78505f5dfe031d67cff82d1338c9780fbfc3cbb
zlazow <zlazow@colgate.edu>
2014-09-28 22:24:16 -0400

    big update

--- list.c
+++ list.c
@@ -1,6 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "list.h"
+#include <string.h>
 
 /* your list function definitions */
+
+/*
+struct node {
+    int number;
+    struct node *next; 
+};
+*/
+
+
+
+void list_append(int num, struct node**head){
+	struct node *new = malloc(sizeof(struct node));
+	new->number = num;
+
+	//if head is null, make the node the head
+	if(*head == NULL) {
+		new->next = *head;
+		*head = new;
+		return;
+	}
+	//if head's val > i, make the head the new value.
+	if ( ((*head)->number) >num){
+		new->next = *head;
+		*head = new;
+		return;
+	}
+	//otherwise iterate through the list, finding a value bigger 
+	//than the node. Make the previous node point to the new node,
+	//and the new node point to the node that was just found to be 		//bigger. 
+	struct node *iterator = *head;
+	struct node *prev = *head;
+	iterator = iterator->next;
+	
+	while(iterator != NULL) {
+		if((iterator->number) > num){
+			break;
+		}
+		prev = iterator;
+		iterator = iterator->next;
+	}
+	//break fixes the case in which the new node is the max.
+	prev->next = new;
+	new->next = iterator;
+}
+
+void list_print(struct node *list) {
+	//cycle through each element and print it out...
+    	while (list != NULL) {
+        	printf("%d\n", list->number);
+        	list = list->next;
+    	}
+}
+
+void list_delete(struct node *list) {
+	//cycle through each element and delete it.
+    	while (list != NULL) {
+        	struct node *tmp = list;
+        	list = list->next;
+        	free(tmp);
+   	}
+}
+
 
--- main.c
+++ main.c
@@ -9,8 +9,44 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-
 #include "list.h"
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <limits.h>
+
+char** tokenify(const char *s) {
+    // input a constant string --> return an array of token strings (that were separated by whitespace)
+	//need to fix if # is beginning of line
+	int count = 0;
+	char *copy = strdup(s);
+	char *token;
+	char **words=malloc(sizeof(char*)*(strlen(s)));
+	token = strtok(copy ," \n\t");
+	while (token!=NULL){
+		//fix case similar to "123#Apple 123"
+		if (strstr(token, "#")==NULL){
+		//if the line contains a #, ignore the rest of the tokens.
+			words[count]=strdup(token);
+			token = strtok(NULL, " \n\t");
+			count++;	
+		}
+		else{
+			if (token[0]=='#'){
+				break;
+			}
+			//slice up to #
+			char *token_hash=strtok(token, "#");
+			words[count]=strdup(token_hash);
+			count++;
+			break;
+		}
+	}
+	words[count]=NULL;
+	free(copy);
+	return words;
+	
+	
+}
 
 void process_data(FILE *input_file) {
     // !! your code should start here.  the input_file parameter
@@ -18,8 +54,69 @@ void process_data(FILE *input_file) {
     // the fgets() C library function.  close it with the fclose()
     // built-in function
 
+/* 
+0) Open file (Already done???)
+1) Read through each line -- fgets().
+2) Tokenify the line. Ignore comments (#) and blanklines.
+	a) If not in comment-->if token == int --> add to link list.
+		i) negative number issue "-"?
+		ii)
 
+	b) Else ignore.
+3) Sort linked list.
+4) Print to command line:
+	a) Linked List
+	b) CPU Resource Statistics (formatted)
+		i) amount of time program spent executing in user space
+		ii) amount of time spent executing in kernel space.
+		
+5) fclose()
 
+*/
+
+//char *fgets(char *str, int n, FILE *stream)
+//how big to make????
+	/*if (input_file!=stdin){
+		fopen(input_file, "r");
+	} */ 
+	printf("Starting..\n");
+	struct node *head=NULL;
+	char line[1024];
+	while (fgets(line, 1024, input_file)!=NULL){
+		//printf("Line: ");
+		//printf("%s", line);
+		char ** tokens = tokenify(line); //not sure if line must be const, or simply will be interpreted as a constant in the function.
+		for (int i = 0; tokens[i]!=NULL; i++){
+			//loop through all indexes in the token...isdigt then....		
+			int is_int = 1;
+			for (int j=0; j<strlen(tokens[i]); j++){
+				if (j==0){
+		//check if first char in a word is negative symbol.
+					if (tokens[i][j]=='-'){
+						continue;
+					}
+				}
+				if (!isdigit(tokens[i][j])){
+					//check negative numbers...
+					//fix if "123-456"
+					is_int=0;
+					break;
+					
+				}
+			}
+	
+			if (is_int){
+				int int_token = atoi(tokens[i]);
+				list_append(int_token, &head);
+			}
+			
+			
+		}
+		free(tokens);
+	}
+	printf("*** List Contents Begin ***\n");
+	list_print(head);
+	list_delete(head);
 
 }
 
@@ -62,6 +159,17 @@ int main(int argc, char **argv) {
      */
     process_data(datafile);
     fclose(datafile);
+	//time stuff;
+    	struct timeval user; 
+    	struct timeval sys;        
+    	struct rusage used;
+    	getrusage(RUSAGE_SELF, &used);
+    	user = used.ru_utime;
+    	sys = used.ru_stime;
+	printf("User time: %f\n", (float)user.tv_sec		+(float)user.tv_usec/1000000);
+    	printf("System time: %f\n", (float)sys.tv_sec+(float)sys.tv_usec/1000000);
+
+
     return 0;
 }
 #endif
